79ddfb736a2a33c508a7c0fb005d266ca31ea5a5
egwspiti <egwspiti@users.noreply.github.com>
2015-09-28 19:21:59 +0300

    Mock web requests

--- spec/imdb_spec.rb
+++ spec/imdb_spec.rb
@@ -6,13 +6,13 @@ module Imdb
     context '.get' do
       let(:id) { 'tt2415458' }
       let(:endpoint) { MovieEndpoint.new(id).tap { |e| e.doc = Fixtures::doc_for_id(id) } }
+      before(:each) { expect(MovieEndpoint).to receive(:new).at_least(:once).and_return(endpoint) }
 
       it 'can get a movie by imdb id' do
         expect(subject.get(id: id)).to be_instance_of(Movie)
       end
 
       it 'prepends \'tt\' to id if needed' do
-        expect(MovieEndpoint).to receive(:new).at_least(:once).with(id).and_call_original
         expect(subject.get(id: 2415458)).to eq subject.get(id: 'tt2415458')
       end
     end
